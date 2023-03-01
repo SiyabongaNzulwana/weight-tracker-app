8befd64eaae8d0bd6efd0a8ce5d682c00f90ab28
Siyabonga Nzulwana <siyabonga@linumlabs.com>
2023-03-02 01:58:53 +0200

    logic to decide what to show

---
+++ src/utils/PrivateRoutes.js
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from 'react-router-dom'
+
+export const PrivateRoutes = (props) => {
+  const user = localStorage.getItem('user')
+  const data = JSON.parse(user)
+
+  return data && data.data.token ? <Outlet /> : <Navigate to='/login' />
+}
