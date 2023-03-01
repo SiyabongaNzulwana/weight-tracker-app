05acfd0ee0c8692772aff22936d4cafd34bd8865
Siyabonga Nzulwana <siyabonga@linumlabs.com>
2023-03-02 01:57:32 +0200

    created protected routes and NAVBAR

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import UserContextProvider from './contexts/UserContext'
+import './App.css'
+import { Home } from './pages/Home'
+import { Progress } from './pages/Progress'
+import { SignUp } from './pages/SignUp'
+import { SignIn } from './pages/SignIn'
+import { SignOut } from './pages/SignOut'
+import { NavBar } from '../src/components/Navbar/Index'
+import { PrivateRoutes } from './utils/PrivateRoutes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <UserContextProvider>
+      <BrowserRouter>
+        <NavBar />
+        <div className='App'>
+          <Routes>
+            <Route element={<PrivateRoutes />}>
+              <Route element={<Home />} path='/' exact />
+              <Route element={<Home />} path='/home' />
+              <Route element={<Progress />} path='/weight-history' />
+            </Route>
+            <Route element={<SignIn />} path='/login' exact />
+            <Route element={<SignUp />} path='/sign_up' exact />
+            <Route element={<SignOut />} path='/logout' exact />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </UserContextProvider>
+  )
 }
 
-export default App;
+export default App
