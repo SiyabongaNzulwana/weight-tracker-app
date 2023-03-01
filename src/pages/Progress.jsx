386f10dbb1886593b514b801127e86a4401ec77a
Siyabonga Nzulwana <siyabonga@linumlabs.com>
2023-03-02 01:59:33 +0200

    created a custom hook

---
+++ src/pages/Progress.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+
+import { WeightList } from '../components/WeightList'
+
+export const Progress = () => {
+  const [weightData, setWeightData] = useState([])
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    const data = localStorage.getItem('user')
+    const user = JSON.parse(data)
+    setUser(user)
+    fetch(
+      `http://localhost:3001/api/users/get_weight_history/${user.data.email}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-auth-token': user.data.token
+        }
+      }
+    )
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        return setWeightData(data.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      {!weightData ? (
+        <div>Loading...</div>
+      ) : (
+        <WeightList weights={weightData} />
+      )}
+    </div>
+  )
+}
